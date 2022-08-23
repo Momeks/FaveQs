3373cce7a7ca776b5b9b82a1e306fc578a62368a
Mohammad Komeili <momeksdev@gmail.com>
2022-08-23 17:03:57 +0430

    Initial Commit

---
+++ FaveQs/FaveQsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaveQsApp.swift
+//  FaveQs
+//
+//  Created by Momeks on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FaveQsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
