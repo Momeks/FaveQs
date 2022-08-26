739a108314e22e46468469c1abc6c64d1b7e6a0b
Mohammad Komeili <momeksdev@gmail.com>
2022-08-26 22:31:46 +0430

    Improved tags

---
+++ FaveQs/Utilities/UI Components/ActionButton.swift
@@ -0,0 +1,20 @@
+//
+//  ActionButton.swift
+//  FaveQs
+//
+//  Created by Momeks on 8/26/22.
+//
+
+import SwiftUI
+
+struct ActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ActionButton()
+    }
+}
---
+++ FaveQs/Views/Quote List/Filtered Quotes/FilteredQuotesView.swift
@@ -0,0 +1,20 @@
+//
+//  FilteredQuotesView.swift
+//  FaveQs
+//
+//  Created by Momeks on 8/26/22.
+//
+
+import SwiftUI
+
+struct FilteredQuotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilteredQuotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilteredQuotesView()
+    }
+}
